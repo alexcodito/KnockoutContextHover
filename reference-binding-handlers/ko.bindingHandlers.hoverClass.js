6bd74f3bd6af58da52135d082a98060f7839b054
Gogs <gogs@fake.local>
2018-12-24 14:20:35 +0000

    ko.bindingHandlers.hoverClass.js - Removed dependency from jQuery

--- reference-binding-handlers/ko.bindingHandlers.hoverClass.js
+++ reference-binding-handlers/ko.bindingHandlers.hoverClass.js
@@ -2,18 +2,16 @@
     ko.bindingHandlers.hoverClass = {
         update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 
-            $(element).on("mouseover",
-                function(e) {
-                    if (e.target === element) {
-                        element.classList.add("class", valueAccessor());
-                        e.stopPropagation();
-                    }
-                });
+            element.addEventListener('mouseover', function (e) {
+                if (e.target === element) {
+                    element.classList.add("class", valueAccessor());
+                    e.stopPropagation();
+                }
+            });
 
-            $(element).on("mouseleave",
-                function(e) {
-                    element.classList.remove("class", valueAccessor());
-                });
+            element.addEventListener('mouseleave', function (e) {
+                element.classList.remove("class", valueAccessor());
+            });
 
         }
     };
